00f25efcd6eb8d7f93175651a4ad75a0f5ba9362
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-08 19:13:29 +1000

    refactor: extract navbar to seperate component

--- apps/web/src/layouts/Layout.tsx
+++ apps/web/src/layouts/Layout.tsx
@@ -1,33 +1,10 @@
 import { Outlet } from "react-router";
-import LogoutButton from "../components/LogoutButton";
+import NavBar from "../components/NavBar";
 
 export default function Layout() {
   return (
     <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/login">Login</a>
-            </li>
-            <li>
-              <a href="/register">Register</a>
-            </li>
-            <li>
-              <a href="/app">App</a>
-            </li>
-            <li>
-              <a href="/secret">Secret</a>
-            </li>
-            <li>
-              <LogoutButton />
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <NavBar />
       <main>
         <Outlet />
       </main>
