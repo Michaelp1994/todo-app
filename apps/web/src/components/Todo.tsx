8bfa08152023921368b841e7658f64ec4ea53a97
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-08 22:09:54 +1000

    feat: add update todo page

--- apps/web/src/components/Todo.tsx
+++ apps/web/src/components/Todo.tsx
@@ -1,11 +1,18 @@
 import type { RouterOutput } from "@todo/api";
 import styles from "./Todo.module.css";
+import UpdateTodoForm from "./UpdateTodoForm";
+import { useState } from "react";
+import Button from "./ui/Button";
 
 interface TodoProps {
   todo: RouterOutput["todo"]["getAll"][0];
 }
 
 export default function Todo({ todo }: TodoProps) {
+  const [isUpdating, setIsUpdating] = useState(false);
+  if (isUpdating) {
+    return <UpdateTodoForm todo={todo} onFinish={() => setIsUpdating(false)} />;
+  }
   return (
     <div className={styles.todo}>
       <h3>{todo.title}</h3>
@@ -13,6 +20,7 @@ export default function Todo({ todo }: TodoProps) {
       <p>Due: {todo.dueDate}</p>
       <p>Completed: {todo.completed ? "Yes" : "No"}</p>
       <p>Important: {todo.important ? "Yes" : "No"}</p>
+      <Button onClick={() => setIsUpdating((value) => !value)}>Update</Button>
     </div>
   );
 }
