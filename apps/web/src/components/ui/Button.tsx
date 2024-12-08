122f75f8e7f56714bd9616eed3d4be57c0fe14e2
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-08 17:26:43 +1000

    feat: add basic styling for button

--- apps/web/src/components/ui/Button.tsx
+++ apps/web/src/components/ui/Button.tsx
@@ -1,7 +1,9 @@
+import styles from "./Button.module.css";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary";
 }
 
 export default function Button(props: ButtonProps) {
-  return <button {...props} />;
+  return <button className={styles.button} {...props} />;
 }
