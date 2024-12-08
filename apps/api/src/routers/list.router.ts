53890379c9d8d53ada3a27a5d5dd7e21a5e9328d
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-08 17:56:52 +1000

    fix: throw error rather than return error, and change endpoints to mutations

--- apps/api/src/routers/list.router.ts
+++ apps/api/src/routers/list.router.ts
@@ -11,32 +11,32 @@ import {
 
 export default router({
   getAll: authProcedure.input(getAllSchema).query(async () => {
-    return new TRPCError({
+    throw new TRPCError({
       code: "NOT_IMPLEMENTED",
     });
   }),
   getById: authProcedure.input(getByIdSchema).query(async () => {
-    return new TRPCError({
+    throw new TRPCError({
       code: "NOT_IMPLEMENTED",
     });
   }),
-  create: authProcedure.input(createSchema).query(async () => {
-    return new TRPCError({
+  create: authProcedure.input(createSchema).mutation(async () => {
+    throw new TRPCError({
       code: "NOT_IMPLEMENTED",
     });
   }),
-  update: authProcedure.input(updateSchema).query(async () => {
-    return new TRPCError({
+  update: authProcedure.input(updateSchema).mutation(async () => {
+    throw new TRPCError({
       code: "NOT_IMPLEMENTED",
     });
   }),
-  archive: authProcedure.input(archiveSchema).query(async () => {
-    return new TRPCError({
+  archive: authProcedure.input(archiveSchema).mutation(async () => {
+    throw new TRPCError({
       code: "NOT_IMPLEMENTED",
     });
   }),
-  delete: authProcedure.input(deleteSchema).query(async () => {
-    return new TRPCError({
+  delete: authProcedure.input(deleteSchema).mutation(async () => {
+    throw new TRPCError({
       code: "NOT_IMPLEMENTED",
     });
   }),
