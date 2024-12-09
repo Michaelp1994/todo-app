e2c495273943acb031e4738559579c1c500f2dcd
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-09 17:44:22 +1000

    feat: add basic global css variables

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import { ApiProvider } from "./contexts/api/ApiProvider";
 import { AuthProvider } from "./contexts/auth/AuthProvider";
 import Router from "./router";
 import "./reset.css";
+import "./globals.css";
 
 const rootElement = document.getElementById("root");
 
