fc02a073f7e9b743518849eaff590bad8d9d2719
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-11 11:33:05 +1000

    feat: add list endpoints

--- apps/api/src/db/schemas/list.ts
+++ apps/api/src/db/schemas/list.ts
@@ -1,13 +1,23 @@
 import type { InferSelectModel } from "drizzle-orm";
-import { text, serial, pgTable, integer } from "drizzle-orm/pg-core";
+import {
+  text,
+  serial,
+  pgTable,
+  integer,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
 import { userTable } from "./user";
 
 export const listTable = pgTable("list", {
   id: serial().primaryKey(),
   title: text().notNull(),
+  slug: uuid().notNull().defaultRandom(),
   userId: integer()
     .notNull()
     .references(() => userTable.id),
+  createdAt: timestamp().notNull().defaultNow(),
+  archivedAt: timestamp(),
 });
 
 export type List = InferSelectModel<typeof listTable>;
--- apps/api/src/routers/list.router.ts
+++ apps/api/src/routers/list.router.ts
@@ -1,4 +1,3 @@
-import { TRPCError } from "@trpc/server";
 import { authProcedure, router } from "../trpc";
 import {
   archiveSchema,
@@ -6,38 +5,77 @@ import {
   deleteSchema,
   getAllSchema,
   getByIdSchema,
+  getBySlugSchema,
   updateSchema,
 } from "../validators/list.schema";
+import { listTable } from "../db/schemas/list";
+import { and, eq } from "drizzle-orm";
 
 export default router({
-  getAll: authProcedure.input(getAllSchema).query(async () => {
-    throw new TRPCError({
-      code: "NOT_IMPLEMENTED",
-    });
+  getAll: authProcedure.input(getAllSchema).query(async ({ ctx }) => {
+    return await ctx.db
+      .select()
+      .from(listTable)
+      .where(eq(listTable.userId, ctx.user.id))
+      .execute();
   }),
-  getById: authProcedure.input(getByIdSchema).query(async () => {
-    throw new TRPCError({
-      code: "NOT_IMPLEMENTED",
-    });
+  getById: authProcedure.input(getByIdSchema).query(async ({ ctx, input }) => {
+    const [list] = await ctx.db
+      .select()
+      .from(listTable)
+      .where(and(eq(listTable.userId, ctx.user.id), eq(listTable.id, input.id)))
+      .execute();
+    return list;
   }),
-  create: authProcedure.input(createSchema).mutation(async () => {
-    throw new TRPCError({
-      code: "NOT_IMPLEMENTED",
-    });
+  getBySlug: authProcedure
+    .input(getBySlugSchema)
+    .query(async ({ ctx, input }) => {
+      const [list] = await ctx.db
+        .select()
+        .from(listTable)
+        .where(
+          and(eq(listTable.userId, ctx.user.id), eq(listTable.slug, input.slug))
+        )
+        .execute();
+      return list;
+    }),
+  create: authProcedure.input(createSchema).mutation(async ({ ctx, input }) => {
+    const [newList] = await ctx.db
+      .insert(listTable)
+      .values({ ...input, userId: ctx.user.id })
+      .returning()
+      .execute();
+    return newList;
   }),
-  update: authProcedure.input(updateSchema).mutation(async () => {
-    throw new TRPCError({
-      code: "NOT_IMPLEMENTED",
-    });
+  update: authProcedure.input(updateSchema).mutation(async ({ ctx, input }) => {
+    const { id, ...values } = input;
+    const [updatedList] = await ctx.db
+      .update(listTable)
+      .set({ ...values })
+      .where(and(eq(listTable.id, id), eq(listTable.userId, ctx.user.id)))
+      .returning()
+      .execute();
+    return updatedList;
   }),
-  archive: authProcedure.input(archiveSchema).mutation(async () => {
-    throw new TRPCError({
-      code: "NOT_IMPLEMENTED",
-    });
-  }),
-  delete: authProcedure.input(deleteSchema).mutation(async () => {
-    throw new TRPCError({
-      code: "NOT_IMPLEMENTED",
-    });
+  archive: authProcedure
+    .input(archiveSchema)
+    .mutation(async ({ ctx, input }) => {
+      const [archivedList] = await ctx.db
+        .update(listTable)
+        .set({ archivedAt: new Date() })
+        .where(
+          and(eq(listTable.id, input.id), eq(listTable.userId, ctx.user.id))
+        )
+        .returning()
+        .execute();
+      return archivedList;
+    }),
+  delete: authProcedure.input(deleteSchema).mutation(async ({ ctx, input }) => {
+    const [deletedList] = await ctx.db
+      .delete(listTable)
+      .where(and(eq(listTable.id, input.id), eq(listTable.userId, ctx.user.id)))
+      .returning()
+      .execute();
+    return deletedList;
   }),
 });
