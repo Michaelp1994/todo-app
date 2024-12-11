1aef3a9a9734a18022f01ed8d427e19aab05943d
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-11 11:47:27 +1000

    feat: add create todo modal

--- apps/web/src/components/forms/CreateTodoForm.tsx
+++ apps/web/src/components/forms/CreateTodoForm.tsx
@@ -3,13 +3,22 @@ import Button from "../ui/Button";
 import Input from "../ui/Input";
 import { api } from "../../utils/api";
 import { Form, FormActions } from "../ui/Form";
+import Checkbox from "../ui/Checkbox";
+import { CalendarMinus, CalendarPlus } from "@phosphor-icons/react";
+import ListSelect from "../ListSelect";
 
-export default function CreateTodoForm() {
+interface CreateTodoFormProps {
+  onFinish(): void;
+}
+
+export default function CreateTodoForm({ onFinish }: CreateTodoFormProps) {
+  const today = new Date().toISOString().split("T")[0];
   const utils = api.useUtils();
   const createMutation = api.todo.create.useMutation({
     async onSuccess() {
-      await utils.todo.getAll.invalidate();
+      await utils.todo.getAllToday.invalidate();
       setMessage("Created!");
+      onFinish();
     },
     onError(error) {
       console.error(error);
@@ -20,21 +29,21 @@ export default function CreateTodoForm() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [important, setImportant] = useState(false);
-  const [dueDate, setDueDate] = useState(
-    new Date().toISOString().split("T")[0]
-  );
+  const [dueDate, setDueDate] = useState<string>(today);
+  const [hasDueDate, setHasDueDate] = useState<boolean>(false);
   const [message, setMessage] = useState("");
+  const [listId, setListId] = useState("");
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     createMutation.mutate({
       title,
-      description,
-      dueDate,
+      description: description ? description : null,
+      dueDate: hasDueDate ? dueDate : null,
       important,
+      listId: listId ? Number(listId) : null,
       completed: false,
       attachmentUrl: "",
-      order: 1,
     });
   }
 
@@ -49,23 +58,38 @@ export default function CreateTodoForm() {
       <Input
         value={description}
         onChange={(e) => setDescription(e.target.value)}
-        label="description"
+        label="Description"
       />
-      <Input
-        value={dueDate}
-        onChange={(e) => setDueDate(e.target.value)}
-        label="due date"
-        type="date"
-      />
-      <Input
+      <ListSelect value={listId} onChange={(e) => setListId(e.target.value)} />
+      {hasDueDate && (
+        <Input
+          disabled={!hasDueDate}
+          value={dueDate}
+          onChange={(e) => setDueDate(e.target.value)}
+          label="Due Date"
+          type="date"
+        />
+      )}
+
+      <Checkbox
         checked={important}
         onChange={() => setImportant((value) => !value)}
         label="Important"
-        type="checkbox"
       />
       {message && <p>{message}</p>}
 
       <FormActions>
+        <Button
+          variant="icon"
+          type="button"
+          onClick={() => setHasDueDate((val) => !val)}
+        >
+          {hasDueDate ? (
+            <CalendarMinus size={24} />
+          ) : (
+            <CalendarPlus size={24} />
+          )}
+        </Button>
         <Button type="submit" disabled={createMutation.isLoading}>
           {createMutation.isLoading ? "Loading..." : "Create"}
         </Button>
