33a52bd2b928cd656ccd60869363a3ea2cfdb525
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-11 11:40:07 +1000

    feat: add chime sound when user completes a todo

--- apps/web/src/components/CompleteTodoButton.tsx
+++ apps/web/src/components/CompleteTodoButton.tsx
@@ -1,6 +1,8 @@
 import { CheckCircle, Circle } from "@phosphor-icons/react";
 import { api } from "../utils/api";
 import Button from "./ui/Button";
+import chime from "/chime.wav";
+import useSound from "use-sound";
 
 interface CompleteTodoButtonProps {
   todoId: number;
@@ -11,10 +13,12 @@ export default function CompleteTodoButton({
   todoId,
   isComplete,
 }: CompleteTodoButtonProps) {
+  const [playChime] = useSound(chime);
   const utils = api.useUtils();
   const completeMutation = api.todo.update.useMutation({
-    async onSuccess() {
-      await utils.todo.getAll.invalidate();
+    async onSuccess({ completed }) {
+      await utils.todo.getAllToday.invalidate();
+      if (completed) playChime();
     },
     onError(error) {
       console.error(error);
