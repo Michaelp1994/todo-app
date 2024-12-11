769f7c69faa3b54ffa4f334e2cc56980f0763336
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-11 12:09:07 +1000

    refactor: add unique constraints on list name (per user)

--- apps/api/src/db/schemas/list.ts
+++ apps/api/src/db/schemas/list.ts
@@ -6,18 +6,23 @@ import {
   integer,
   timestamp,
   uuid,
+  unique,
 } from "drizzle-orm/pg-core";
 import { userTable } from "./user";
 
-export const listTable = pgTable("list", {
-  id: serial().primaryKey(),
-  title: text().notNull(),
-  slug: uuid().notNull().defaultRandom(),
-  userId: integer()
-    .notNull()
-    .references(() => userTable.id),
-  createdAt: timestamp().notNull().defaultNow(),
-  archivedAt: timestamp(),
-});
+export const listTable = pgTable(
+  "list",
+  {
+    id: serial().primaryKey(),
+    title: text().notNull(),
+    slug: uuid().notNull().defaultRandom(),
+    userId: integer()
+      .notNull()
+      .references(() => userTable.id),
+    createdAt: timestamp().notNull().defaultNow(),
+    archivedAt: timestamp(),
+  },
+  (t) => [unique().on(t.userId, t.title)]
+);
 
 export type List = InferSelectModel<typeof listTable>;
